e15303c6b2c08e7879d6f34f00be465d300b3134
Jim Safley <jimsafley@gmail.com>
2012-01-27 12:13:37 -0500

    Sanitize constraints in process. Minor code changes.

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -3,13 +3,6 @@ class ImageResize_IndexController extends Omeka_Controller_Action
 {
     public function indexAction()
     {
-        // Only resize images on systems using the filesystem storage adapter.
-        if (Zend_Registry::get('storage')->getAdapter() instanceof Omeka_Storage_Adapter_Filesystem) {
-            $this->view->assign('usingFilesystemAdapter', true);
-        } else {
-            $this->view->assign('usingFilesystemAdapter', false);
-        }
-        
         if (isset($_POST['image_resize_submit'])) {
             
             $constraints = array('fullsize_constraint' => false, 
@@ -24,7 +17,7 @@ class ImageResize_IndexController extends Omeka_Controller_Action
                 }
                 
                 // Validate the form.
-                if (!preg_match('/^[1-9][0-9]*$/', $_POST[$key])) {
+                if (!is_numeric($_POST[$key]) || !preg_match('/^[1-9][0-9]*$/', $_POST[$key])) {
                     $this->flashError('Size constraints must be positive integers. No images were resized.');
                     $validationError = true;
                     break;
@@ -57,6 +50,14 @@ class ImageResize_IndexController extends Omeka_Controller_Action
             }
         }
         
+        // Only resize images on systems using the filesystem storage adapter.
+        if (Zend_Registry::get('storage')->getAdapter() instanceof Omeka_Storage_Adapter_Filesystem) {
+            $this->view->assign('usingFilesystemAdapter', true);
+        } else {
+            $this->view->assign('usingFilesystemAdapter', false);
+        }
+        
+        // Set the previous image resize processes.
         $processes = $this->getTable('Process')->findByClass('ImageResizeProcess');
         $this->view->assign('processes', $processes);
     }
--- models/ImageResizeProcess.php
+++ models/ImageResizeProcess.php
@@ -1,6 +1,10 @@
 <?php
 class ImageResizeProcess extends ProcessAbstract
 {
+    protected $_derivativeTypes = array('fullsize_constraint', 
+                                        'thumbnail_constraint', 
+                                        'square_thumbnail_constraint');
+    
     public function run($constraints)
     {
         $db = get_db();
@@ -11,35 +15,46 @@ class ImageResizeProcess extends ProcessAbstract
             throw new Exception('The storage adapter is not an instance of Omeka_Storage_Adapter_Filesystem.');
         }
         
+        // Sanitize the constraints.
+        foreach ($constraints as $key => $value) {
+            if (!in_array($key, $this->_derivativeTypes) 
+             || !is_numeric($value) 
+             || !preg_match('/^[1-9][0-9]*$/', $value)) {
+                unset($constraints[$key]);
+            }
+        }
+        
         // Iterate all image files in the archive.
         $sql = "SELECT * FROM {$db->File} WHERE has_derivative_image = 1";
         foreach ($db->query($sql)->fetchAll() as $imageFile) {
             
             // Iterate the constraints.
-            foreach ($constraints as $key => $constraint) {
-                if (!$constraint) {
-                    continue;
-                }
-                $oldPath = FILES_DIR . '/' . $imageFile['archive_filename'];
-                switch ($key) {
+            foreach ($constraints as $derivativeType => $constraint) {
+                
+                $filesPath = FILES_DIR . '/' . $imageFile['archive_filename'];
+                
+                switch ($derivativeType) {
+                    // Resize square thumbnail.
                     case 'square_thumbnail_constraint':
-                        $newFileName = Omeka_File_Derivative_Image::createImage($oldPath, $constraint, 'square_thumbnail');
+                        $newFileName = Omeka_File_Derivative_Image::createImage($filesPath, $constraint, 'square_thumbnail');
                         $source = FILES_DIR . '/square_thumbnail_' . $newFileName;
                         $dest = $storage->getPathByType($newFileName, 'square_thumbnails');
                         break;
+                    // Resize thumbnail.
                     case 'thumbnail_constraint':
-                        $newFileName = Omeka_File_Derivative_Image::createImage($oldPath, $constraint, 'thumbnail');
+                        $newFileName = Omeka_File_Derivative_Image::createImage($filesPath, $constraint, 'thumbnail');
                         $source = FILES_DIR . '/thumbnail_' . $newFileName;
                         $dest = $storage->getPathByType($newFileName, 'thumbnails');
                         break;
-                    case 'fullsize_constraint':
-                        $newFileName = Omeka_File_Derivative_Image::createImage($oldPath, $constraint, 'fullsize');
+                    // Resize fullsize.
+                    default:
+                        $newFileName = Omeka_File_Derivative_Image::createImage($filesPath, $constraint, 'fullsize');
                         $source = FILES_DIR . '/fullsize_' . $newFileName;
                         $dest = $storage->getPathByType($newFileName, 'fullsize');
                         break;
-                    default:
-                        break;
                 }
+                
+                // Store the file.
                 $storage->store($source, $dest);
             }
         }
