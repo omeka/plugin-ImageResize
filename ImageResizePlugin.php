fb5a7efedd91f9be3ee10dcb5b855d1ddd6f1238
Jim Safley <jimsafley@gmail.com>
2012-02-01 17:19:56 -0500

    Restrict access to super and admin.

--- ImageResizePlugin.php
+++ ImageResizePlugin.php
@@ -1,7 +1,15 @@
 <?php
 class ImageResizePlugin extends Omeka_Plugin_Abstract
 {
+    protected $_hooks = array('define_acl');
+    
     protected $_filters = array('admin_navigation_main');
+    
+    public function hookDefineAcl($acl)
+    {
+        $resourceList = array('ImageResize_Index' => array('index', 'resize'));
+        $acl->loadResourceList($resourceList);
+    }
     
     public function filterAdminNavigationMain($nav)
     {
