a2c11d61c4afaaf81eb5e087eb15a43465ac4c09
Patrick Murray-John <patrickmjchnm@gmail.com>
2012-02-27 13:31:57 -0500

    Makes compatible with 2.0 Acl

--- ImageResizePlugin.php
+++ ImageResizePlugin.php
@@ -2,15 +2,19 @@
 class ImageResizePlugin extends Omeka_Plugin_Abstract
 {
     protected $_hooks = array('define_acl');
-    
+
     protected $_filters = array('admin_navigation_main');
-    
+
     public function hookDefineAcl($acl)
     {
-        $resourceList = array('ImageResize_Index' => array('index', 'resize'));
-        $acl->loadResourceList($resourceList);
+        if (version_compare(OMEKA_VERSION, '2.0-dev', '>=')) {
+            $acl->addResource('ImageResize_Index');
+        } else {
+            $resourceList = array('ImageResize_Index' => array('index', 'resize'));
+            $acl->loadResourceList($resourceList);
+        }
     }
-    
+
     public function filterAdminNavigationMain($nav)
     {
         $nav['Image Resize'] = uri('image-resize');
